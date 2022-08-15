eff829e459a573a7a462762121ce4477f43d2fef
Steven <slosch99@gmail.com>
2022-08-15 17:00:55 -0400

    Completed

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
 
+class Book
+    attr_accessor :book, :author, :genre, :page_count, :title , :turn_page
+    # attr_reader :turn_page
+    def initialize(title)
+        @title = title
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
 
